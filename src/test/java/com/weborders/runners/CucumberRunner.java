8ef5801e2b7c45433e3e99e1d48361e7b2f959a6
SCybertek <snuryyeva@gmail.com>
2020-04-22 21:11:05 -0400

    first commit

---
+++ src/test/java/com/weborders/runners/CucumberRunner.java
@@ -0,0 +1,24 @@
+package com.weborders.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(
+        glue = "com/vytrack/step_definitions",
+        features = "src/test/resources",
+        dryRun = false,
+        strict = false,
+        tags = "@scenario_outline_2",
+        plugin = {"html:target/default-report",
+                "json:target/cucumber1.json"
+        }
+
+)
+public class CucumberRunner {
+
+
+}
